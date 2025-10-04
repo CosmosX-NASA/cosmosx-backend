7494d2420c0fb57a018f4a24b8a82ff461b343e9
jaewonE <importjaewonE@gmail.com>
2025-10-05 01:16:57 +0900

    [Feat] Add Dataset to Database.db

--- model/research_figure_model.py
+++ model/research_figure_model.py
@@ -7,10 +7,11 @@ from db.db_base import db_base
 class Figure(db_base):
     __tablename__ = "figure"
 
-    id = Column(Integer, primary_key=True, index=True, autoincrement=True, comment="논문 PK")
-    url = Column(String, unique=True, nullable=False, comment = "이미지 url")
-    caption = Column(String, unique=True, nullable=True, comment = "이미지 캡션")
-    research_id = Column(Integer, nullable=False, comment= "연구 논문 id")
+    id = Column(Integer, primary_key=True, index=True,
+                autoincrement=True, comment="논문 PK")
+    url = Column(String, unique=False, nullable=False, comment="이미지 url")
+    caption = Column(String, unique=False, nullable=True, comment="이미지 캡션")
+    research_id = Column(Integer, nullable=False, comment="연구 논문 id")
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
