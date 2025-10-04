95da4399cc5217759df42050897346eca1eaf3e6
JaeWonKawk <69011834+jaewonE@users.noreply.github.com>
2025-10-05 03:26:19 +0900

    [Feat] Insert gap data (#33)
    * [Feat] Add research repository
    
    * [Feat] Add Dataset to Database.db
    
    * [Refactor] Remove debug print statements for PMC IDs in ResearchRagRepository
    
    * [Refactor] Add new line to cfg.json
    
    * [Feat] Add gap data insert system
    
    * [Remove] Remove Database.db

--- db/db.py
+++ db/db.py
@@ -3,6 +3,8 @@ import os
 from sqlalchemy import create_engine, event
 from sqlalchemy.orm import sessionmaker
 from .db_base import db_base
+import subprocess
+import time
 
 # 모델을 메타데이터에 등록하기 위해 import가 필요.
 from model import Research, Figure, ResearchGap, Hypothesis, HypothesisResearch  # noqa: F401
@@ -32,6 +34,33 @@ try:
 except Exception as e:
     print(f"테이블 생성 실패: {e}")
 
+try:
+    time.sleep(1)  # 잠시 대기
+    populate_sql_path = os.path.join(os.getcwd(), "populate.sql")
+    if os.path.exists(populate_sql_path):
+        with open(populate_sql_path, "rb") as sql_file:
+            subprocess.run([
+                "sqlite3",
+                DB_PATH,
+            ], stdin=sql_file, check=True)
+        print("기본 데이터 삽입 완료(1/2)")
+    else:
+        print(f"populate.sql 파일을 찾을 수 없습니다: {populate_sql_path}")
+
+    time.sleep(1)  # 잠시 대기
+    populate_sql_path = os.path.join(os.getcwd(), "insert_research_gaps.sql")
+    if os.path.exists(populate_sql_path):
+        with open(populate_sql_path, "rb") as sql_file:
+            subprocess.run([
+                "sqlite3",
+                DB_PATH,
+            ], stdin=sql_file, check=True)
+        print("기본 데이터 삽입 완료(2/2)")
+    else:
+        print(f"insert_research_gaps.sql 파일을 찾을 수 없습니다: {populate_sql_path}")
+except Exception as e:
+    print(f"기본 데이터 삽입 실패: {e}")
+
 # 5. 세션 생성기 설정
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
@@ -49,4 +78,4 @@ def get_db_session():
     finally:
         db.close()
 
-#jdbc:sqlite:/Users/coli/Desktop/projects/fast-api-practice/Database.db
+# jdbc:sqlite:/Users/coli/Desktop/projects/fast-api-practice/Database.db
