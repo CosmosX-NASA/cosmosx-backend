20cf7160d072d4d42ab51f3f2032229c55d448e2
coli <kkwoo001021@naver.com>
2025-10-04 11:15:16 +0900

    chore: test 패키지 생성

--- main.py
+++ main.py
@@ -1,15 +1,12 @@
+
 from fastapi import FastAPI
+from apis.ping_api import router as ping_router
 
-app = FastAPI()
+app = FastAPI(title="Practice API", version="1.0.0")
 
+app.include_router(ping_router)
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
-
-
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
-
-# python3 -m uvicorn main:app --host 0.0.0.0 --port 7701 --reload
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=7071)
+# python3 -m uvicorn main:app --host 0.0.0.0 --port 8000 --reload
