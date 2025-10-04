bbcd7d9555b653b30fbaf84ef2a14e0febb68f7f
coli <kkwoo001021@naver.com>
2025-10-04 10:28:01 +0900

    chore: 환경 세팅

---
+++ main.py
@@ -0,0 +1,15 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+
+@app.get("/hello/{name}")
+async def say_hello(name: str):
+    return {"message": f"Hello {name}"}
+
+# python3 -m uvicorn main:app --host 0.0.0.0 --port 7701 --reload
